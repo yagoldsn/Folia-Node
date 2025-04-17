d225ca516513e009c4030d11e21bd1738ef3e150
yagoldsn <yagoldsn@gmail.com>
2025-04-17 02:08:21 -0300

    implementado validação do helpers inicial para navbar administrador e cliente primeira

--- index.js
+++ index.js
@@ -6,12 +6,43 @@ const bodyParser = require("body-parser");
 const banco = require("./SRC/config/DB");
 const path = require("path"); //modulo para usar pasta de arquivos estaticos
 const admin = require("./SRC/router/admin");
+const session =require ('express-session');
+const flash  = require('connect-flash');
+
 
 //config
+
+//Sessão
+app.use(session({
+  secret:"foliacaboclo",
+  resave:true,
+  saveUninitialized:true
+}))
+//Flash
+app.use(flash());
+//middleware
+app.use((req,res,next) =>{
+  //declaração variaveis globais
+  res.locals.usuarioLogado = 'cliente';
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  next();
+})
+
 //handlebars template engine
 const optionsHandlebars = handlebars.create({
   defaultLayout: "main",
+  helpers: {
+    /* Para usar o helpers basta adicionar #nomeDaRegra dentro da tag a utilizar e fechar ela depois de utilizada
+    ex: <h1>{{#tipoPessoa teste}}{{/tipoPessoa}}</h1>
+    */
+    checked: function (value, test) {//checked nome função, value e teste sao os valores para testar
+      if (value == undefined) return "";//validação de valor não definido, retornar vazio
+      return value == test ? value : "";//validação se value == teste; retornar se verdadeiro; retornar se falso
+    }
+  }
 });
+
 app.engine("handlebars", optionsHandlebars.engine);
 app.set("view engine", "handlebars");
 //body-parser
