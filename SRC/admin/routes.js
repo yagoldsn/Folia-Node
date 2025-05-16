d1976057a42d96329fa65a59aadec2ae59815658
yago <yagoldsn@gmail.com>
2025-05-15 22:58:03 -0300

    Ajustes Layout tela de cadastros

--- SRC/admin/routes.js
+++ SRC/admin/routes.js
@@ -5,10 +5,12 @@ router.get("/", (req, res) => {
   res.render("admin/index");
 });
 
+router.get("/cadastro", (req, res) => {
+  res.render("admin/cadastros");
+});
 
 // router.get("/usuarios/cadastro", (req, res) => {
 //   res.render("usuarios/cadastrar");
 // });
 
 module.exports = router;
-
